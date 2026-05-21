0d70ac910e796b4cdac6f0ccc5477c3310f9f443
Ben Ahrdt <github@ben-schmidt.net>
2026-05-21 17:06:46 +0200

    struktur bereinigt

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 import express from "express"
-import cors from "cors"
 
 const app = express()
 
-app.use(cors())
 app.use(express.json())
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
 
     res.json({
         status: "ProjectBuilder API läuft"
